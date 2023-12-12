7e75b06e51e5bdb95a4ffee61068011543f61724
Phu <33654804+lequangphu@users.noreply.github.com>
2023-12-12 15:25:22 +0700

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // Understand the problem: given an array, and arguments to remove from the array, return a new array without the elements.
+    let outputArray = [];
 
+    array.forEach((element) => {
+        if (!args.includes(element)) {
+            outputArray.push(element);
+        }
+    })
+
+    return outputArray;
 };
 
 // Do not edit below this line
