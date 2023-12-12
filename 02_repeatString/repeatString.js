e8153f883e0036b438fda6991bb2299f9a5e64e1
Phu <33654804+lequangphu@users.noreply.github.com>
2023-12-12 10:44:50 +0700

    Finish 01 and 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = '';
 
+    if (num === 0) {
+        return repeatedString;
+    } else if (num < 0) {
+        return 'ERROR';
+    } else {
+        let i = 0;
+
+        do {
+            repeatedString += string;
+            i++;
+        } while (i < num)
+        
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
