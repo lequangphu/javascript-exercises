707cd95434bf0c3610e18dec07ae2630698d6f98
Phu <33654804+lequangphu@users.noreply.github.com>
2023-12-13 14:37:14 +0700

    Finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDegree) {
+  let cDegree = (fDegree - 32) * 5/9;
+  
+  return parseFloat(cDegree.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cDegree) {
+  let fDegree = cDegree * 9/5 + 32;
+
+  return parseFloat(fDegree.toFixed(1));
 };
 
 // Do not edit below this line
