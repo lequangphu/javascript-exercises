8b4c56a2781a9a8021273e1a094647283c607864
Phu <33654804+lequangphu@users.noreply.github.com>
2023-12-13 12:03:48 +0700

    Finish 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,29 @@
-const leapYears = function() {
+// UNDERSTAND the problem
+// Determine whether or not a year is a leap year.
 
+// PLANNING to solve the problem
+// Conditions for a year to be a leap year: (1) divisible by 4; (2) not divisible by 100; (3) divisible by 400.
+
+// PSEUDOCODE
+// GET Year
+// IF Year is divisible by 4
+    // IF Year is not divisible by 100
+        // RETURN true
+    // ELSE IF Year is divisible by 400
+        // RETURN true
+// END IF
+// RETURN false
+
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100) {
+            return true;
+        } else if (year % 400 === 0) {
+            return true;
+        }
+    }
+
+    return false;
 };
 
 // Do not edit below this line
