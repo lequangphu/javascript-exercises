d704c91b65f23ad65dd4907830b2d067a992bf67
Phu <33654804+lequangphu@users.noreply.github.com>
2023-12-13 15:25:26 +0700

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.length === 0 ? 0 : array.reduce((accumulator, currentValue) => accumulator + currentValue);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length === 0 ? 0 : array.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(numRaise, numPower) {
+  return numRaise ** numPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let outputFactorial = 1;
+
+  if (num <= 1) return outputFactorial;
+
+  let i = 2;
+  do {
+    outputFactorial *= i;
+    i++
+  } while (i <= num)
+
+  return outputFactorial;
 };
 
 // Do not edit below this line
