89c832bc1234289069dc66b308925ef4227d1d12
Phu <33654804+lequangphu@users.noreply.github.com>
2023-12-14 13:07:01 +0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currentDay = new Date();
+    const currentYear = currentDay.getFullYear();
+    let theOldest = {};
+    let maxAge = 0;
 
+    peopleArray.forEach(person => {
+        let age;
+
+        if (person.yearOfDeath === undefined) {
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (age > maxAge) {
+            maxAge = age;
+            theOldest = person;
+        }
+    });
+
+    return theOldest;
 };
 
 // Do not edit below this line
