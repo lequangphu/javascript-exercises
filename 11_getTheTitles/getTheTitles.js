7479f3284dc0f5624c5e8c4174185eb95501dd98
Phu <33654804+lequangphu@users.noreply.github.com>
2023-12-14 11:51:34 +0700

    Finish 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    if (bookArray.length === 0) return [];
 
+    let titleArray = [];
+    bookArray.forEach(book => {
+        titleArray.push(book['title']);
+    });
+
+    return titleArray;
 };
 
 // Do not edit below this line
